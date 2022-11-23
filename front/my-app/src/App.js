507512ddb0e0ce3fac487e0e07aebd3a2b1f7fa6
francisco abba <fabba@ithreexglobal.com>
2022-11-23 17:42:27 -0300

    cambios

--- front/my-app/src/App.js
+++ front/my-app/src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import logo from './imagenes/logo1.png';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>bienvenidos a la pagina de ithreex</h1>
-        <h2>bienvenidos a la pagina de ithreex</h2>
-        <h3>bienvenidos a la pagina de ithreex</h3>
+      <div className="logo">
+                <img src={logo} alt="logo" width="200" />
+            </div>
       </header>
       <body className="app-body">
-        <h4>la concha de tu madre all boys</h4>
-        <button>presione aqui</button>
+        <h1>Bienvenido al sistema de ithreex</h1>
+        <br></br>
+        <div className="botones">
+        <ul>
+        <li><button className="boton">Agregar cv</button></li>
+        <li><button className="boton">Modificar cv</button></li>
+        <li><button className="boton">Vacaciones</button></li>
+        </ul>
+        </div>
       </body>
       <footer>
         <h4>
