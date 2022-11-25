18d0ada2105f2f4247bbbe435eef2374ff832ddd
NicoRojo562 <2000262@ucc.edu.ar>
2022-11-25 09:56:21 -0300

    se agrego fondo

--- front/my-app/src/App.js
+++ front/my-app/src/App.js
@@ -1,8 +1,10 @@
 
 import './App.css';
 import logo from './imagenes/logo1.png';
+
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
       <div className="logo">
